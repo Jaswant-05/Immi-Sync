5691ff6e2cddf3ecd552efab6636d9a32015f36d
Jaswant-05 <sandhusjaswant1@gmail.com>
2025-03-28 21:13:32 -0400

    improved folder structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return(
+        <div>
+            <div>
+                <h1>Landing Page</h1>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+export const SignIn = () => {
+    return (
+        <div className="absolute inset-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="border border-black w-full h-full">Log In</div>
+        </div>
+    )
+}
